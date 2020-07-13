cace93f997415694f77d5ca2fae1dba97a5983fe
Michael <53924929+chenxianganjie@users.noreply.github.com>
2020-07-13 08:50:35 +0800

    这是7月13日的提交

--- jtworkspace/jt/jt-manage/src/main/java/com/jt/SpringBootRun.java
+++ jtworkspace/jt/jt-manage/src/main/java/com/jt/SpringBootRun.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRun {
 
     public static void main(String[] args) {
-
+        System.out.println("空");
         SpringApplication.run(SpringBootRun.class, args);
     }
 }
